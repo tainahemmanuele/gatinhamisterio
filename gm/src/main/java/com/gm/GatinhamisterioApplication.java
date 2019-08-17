44fb00e6ff85de083f21bc740f61e6c9bd70d8e2
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-08-17 18:54:04 -0300

    Inicio do projeto

---
+++ gm/src/main/java/com/gm/GatinhamisterioApplication.java
@@ -0,0 +1,13 @@
+package com.gm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatinhamisterioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatinhamisterioApplication.class, args);
+	}
+
+}
